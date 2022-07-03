874ed0d4b39a95dbd83601faef6b1d97acc2dfaf
Iucism <105166639+Iucism@users.noreply.github.com>
2022-07-03 00:09:04 -0400

    Create index.js

---
+++ NoReplyMention/index.js
@@ -0,0 +1,29 @@
+const GLOBAL_NAME = "NoReplyMention";
+
+const { showToast, WebpackModules, Patcher } = VApi;
+
+module.exports = {
+    Plugin: new (class {
+        config = {
+            info: {
+                name: GLOBAL_NAME,
+                description: "Port of NoReplyMention to Velocity",
+            },
+            author: "TheCommieAxolotl",
+            authorId: "538487970408300544",
+            version: "1.0.0",
+            license: "MIT",
+        };
+        getName() {
+            return this.config.info.name;
+        }
+        async onStart() {
+            Patcher.before(GLOBAL_NAME, WebpackModules.find(["createPendingReply"]), "createPendingReply", (data, e) => {
+                data[0].shouldMention = false;
+            });
+        }
+        onStop() {
+            Patcher.unpatchAll(GLOBAL_NAME);
+        }
+    })(),
+};
